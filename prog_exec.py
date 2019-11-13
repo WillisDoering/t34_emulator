92c14f09f6be665fd201b199909284e038e7173b
Doering, William <william.doering@mines.sdsmt.edu>
2019-11-12 21:39:26 -0700

    Reconized TODO comments XXX

--- prog_exec.py
+++ prog_exec.py
@@ -8,7 +8,9 @@ def prog_run(e_mem, user_in):
 # Spacer  |123456789|123456789|123456789|123456789|123456789|
     print("PC  OPC  INS   AMOD OPRND  AC  XR YR SP NV-BDIZC")
 
+    # TODO: Add variables for all values in all fields printed
+
     # Run program until break
     while e_mem.pc != 0:
-        # run opcode and update pc
-        e_mem.pc = 0    # remove when functional
+        # TODO: run opcode and update pc
+        e_mem.pc = 0    # TODO: remove when functional
