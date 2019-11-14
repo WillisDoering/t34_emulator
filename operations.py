611cccb25376c6e4ed2318cf4b05fc444429a459
Doering, William <william.doering@mines.sdsmt.edu>
2019-11-14 15:57:34 -0700

    Created print function for emulator display

--- operations.py
+++ operations.py
@@ -1,24 +1,21 @@
-def asl_a(e_mem):
-    pc = e_mem.pc
-    e_mem.pc += 1
-
-    print('{:03X}'.format(pc),
-          " 0A  ASL      A -- -- ",
+def op_print(pc, op, ins, amod, oprnd, e_mem):
+    print('{:03X}'.format(pc), '', op, '', ins, ' ', amod, oprnd, '',
           '{:02X}'.format(e_mem.registers[0]),
           '{:02X}'.format(e_mem.registers[1]),
           '{:02X}'.format(e_mem.registers[2]),
           '{:02X}'.format(e_mem.registers[4]),
           '{:08b}'.format(e_mem.registers[3]))
+
+
+def asl_a(e_mem):
+    pc = e_mem.pc
+    e_mem.pc += 1
+
+    op_print(pc, "0A", "ASL", "   A", "-- --", e_mem)
 
 
 def nop(e_mem):
     pc = e_mem.pc
     e_mem.pc += 1
 
-    print('{:03X}'.format(pc),
-          " 0A  NOP   impl -- -- ",
-          '{:02X}'.format(e_mem.registers[0]),
-          '{:02X}'.format(e_mem.registers[1]),
-          '{:02X}'.format(e_mem.registers[2]),
-          '{:02X}'.format(e_mem.registers[4]),
-          '{:08b}'.format(e_mem.registers[3]))
+    op_print(pc, "EA", "NOP", "impl", "-- --", e_mem)
