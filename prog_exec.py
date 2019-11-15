c5ab95d7b7a14168d4d817e82454073fc3fcd08f
Doering, William <william.doering@mines.sdsmt.edu>
2019-11-14 22:18:06 -0700

    Operation Completed: TXS (9A)

--- prog_exec.py
+++ prog_exec.py
@@ -18,7 +18,7 @@ opc_table = {
     136: op.dey,        # 88
     138: op.txa,        # 8A
     152: op.tya,        # 98
-
+    154: op.txs,        # 9A
     168: op.tay,        # A8
     170: op.tax,        # AA
     184: op.clv,        # B8
