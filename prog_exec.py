87556a500aae003847bc90ddf18135155e2df492
Doering, William <william.doering@mines.sdsmt.edu>
2019-11-14 18:27:05 -0700

    Minor format changes

--- prog_exec.py
+++ prog_exec.py
@@ -2,15 +2,15 @@ import memory as mem
 import operations as op
 
 opc_table = {
-    0: op.brk,      # 00
-    10: op.asl_a,   # 0A
-    72: op.pha,     # 48
-    104: op.pla,    # 68
-    138: op.txa,    # 8A
-    152: op.tya,    # 98
-    200: op.iny,    # C8
-    232: op.inx,    # E8
-    234: op.nop,    # EA
+    0:   op.brk,        # 00
+    10:  op.asl_a,      # 0A
+    72:  op.pha,        # 48
+    104: op.pla,        # 68
+    138: op.txa,        # 8A
+    152: op.tya,        # 98
+    200: op.iny,        # C8
+    232: op.inx,        # E8
+    234: op.nop,        # EA
 }
 
 
@@ -28,5 +28,3 @@ def prog_run(e_mem, user_in):
 
         func = opc_table.get(opc, op.inv_error)
         cont = func(e_mem)
-
-        # TODO: run opcode and update pc
